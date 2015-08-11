382b69074e31d9a57f55c712d253968c5d58cf6d
cuipengfei <cuipengfei2008@gmail.com>
2015-08-11 16:56:39 +0800

    [Cui Pengfei] 第4：银行不应该接受不合法用户

--- CuiPengfei/test/com/tw/beach/entity/BankTest.java
+++ CuiPengfei/test/com/tw/beach/entity/BankTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.Date;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class BankTest {
@@ -11,5 +12,11 @@ public class BankTest {
     public void bankShouldAcceptValidCustomer() throws Exception {
         Customer laozhang = Customer.createCustomer("laozhang", new Date());
         assertTrue(new Bank().addCustomer(laozhang));
+    }
+
+    @Test
+    public void bankShouldNotAcceptInvalidCustomer() throws Exception {
+        Customer invalidLaoZhang = Customer.createCustomer("!@#laozhang", new Date());
+        assertFalse(new Bank().addCustomer(invalidLaoZhang));
     }
 }

1bb694a359cac43cdf56aaa2c552aee74a1f069c
cuipengfei <cuipengfei2008@gmail.com>
2015-08-11 16:51:24 +0800

    [Cui Pengfei] 第2：使用null object来表达不合法的用户的含义

--- CuiPengfei/src/com/tw/beach/entity/Customer.java
+++ CuiPengfei/src/com/tw/beach/entity/Customer.java
@@ -1,8 +1,13 @@
 package com.tw.beach.entity;
 
 import java.util.Date;
+import java.util.regex.Pattern;
+
+import static java.util.regex.Pattern.compile;
 
 public class Customer {
+    private static final Customer invalidCustomer = new Customer(null, null);
+
     private final String nickName;
     private final Date birthDay;
 
@@ -11,11 +16,22 @@ public class Customer {
         this.birthDay = birthDay;
     }
 
-    public static Customer createCustomer(String nickName, Date date) {
-        return new Customer(nickName, date);
-    }
-
     public String nickName() {
         return nickName;
     }
+
+    public static Customer createCustomer(String nickName, Date date) {
+        Pattern validNamePattern = compile("^[a-z0-9]+$");
+        Boolean isValidName = validNamePattern.matcher(nickName).matches();
+        if (isValidName) {
+            return new Customer(nickName, date);
+        } else {
+            return invalidCustomer;
+        }
+    }
+
+    public static Customer invalidCustomer() {
+        return invalidCustomer;
+    }
+
 }
--- CuiPengfei/test/com/tw/beach/entity/CustomerTest.java
+++ CuiPengfei/test/com/tw/beach/entity/CustomerTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.Date;
 
 import static com.tw.beach.entity.Customer.createCustomer;
+import static com.tw.beach.entity.Customer.invalidCustomer;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -12,6 +13,12 @@ public class CustomerTest {
     @Test
     public void shouldCreateInstanceOfCustomer() throws Exception {
         Customer zhangsan123 = createCustomer("zhangsan123", new Date());
-        assertThat(zhangsan123.nickName(),is("zhangsan123"));
+        assertThat(zhangsan123.nickName(), is("zhangsan123"));
+    }
+
+    @Test
+    public void shouldNotCreateInstanceOfCustomerWhenNameInvalid() throws Exception {
+        Customer zhangsan123 = createCustomer("zhangsan123!!!", new Date());
+        assertThat(zhangsan123, is(invalidCustomer()));
     }
 }

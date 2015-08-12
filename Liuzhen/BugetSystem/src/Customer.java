b14a97065bbb4c06e3b69c400009bdec1f60cbc6
Zhen Liu <zhenliu@CNzhenliu.local>
2015-08-12 14:54:41 +0800

    balance

--- Liuzhen/BugetSystem/src/Customer.java
+++ Liuzhen/BugetSystem/src/Customer.java
@@ -1,25 +1,49 @@
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by zhenliu on 8/11/15.
  */
 public class Customer {
-    private String nickName;
-    private Date dateOfBirth;
+    private static final Customer invalidCustomer = new Customer(null,null);
 
-    public Customer(String _nickName, String _dateOfBirth) throws ParseException {
+    private final String nickName;
+    private final Date dateOfBirth;
+    private double account = 0.0;
+
+    private Customer(String _nickName, Date _dateOfBirth)  {
         nickName = _nickName;
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        dateOfBirth = sdf.parse(_dateOfBirth);
+        dateOfBirth = _dateOfBirth;
+    }
+    public static Customer getInvalidCustomer() {return invalidCustomer;}
+
+    public static Customer createCustomer(String _nickName, Date _dateOfBirth) {
+        if (isValidNickName(_nickName))
+            return new Customer(_nickName, _dateOfBirth);
+        else
+            return invalidCustomer;
+    }
+
+    private static boolean isValidNickName(String _nickName) {
+        String reg = "^[a-z0-9]+$";
+        Pattern pattern = Pattern.compile(reg);
+        Matcher matcher = pattern.matcher(_nickName);
+        Boolean isValidNickName = matcher.matches();
+
+        return isValidNickName;
     }
 
     public String getNickName(){
         return nickName;
     }
 
-    public Date getDateOfBirth(){
-        return dateOfBirth;
+    public void setAccount(double _money) {
+        this.account = _money;
     }
+
+    public double getAccount() {
+        return account;
+    }
+
 }

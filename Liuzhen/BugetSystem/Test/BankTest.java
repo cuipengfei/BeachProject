170f4c5bcc4cfbc19cad17abd47e70bcad5e98ef
Zhen Liu <zhenliu@CNzhenliu.local>
2015-08-13 00:24:45 +0800

    addTest1. should_get_a_blank_account_when_a_customer_be_added_in_a_bank

--- Liuzhen/BugetSystem/Test/BankTest.java
+++
@@ -1,8 +0,0 @@
-import junit.framework.TestCase;
-
-/**
- * Created by zhenliu on 8/12/15.
- */
-public class BankTest extends TestCase {
-
-}
---
+++ Liuzhen/BugetSystem/Test/CustomerTest.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * Created by zhenliu on 8/13/15.
+ */
+public class CustomerTest {
+    @Test
+    public void should_get_a_blank_account_when_a_customer_be_added_in_a_bank() throws Exception {
+        Bank bank = new Bank();
+        Customer customer = Customer.createCustomer("liuzhen11",new Date());
+
+        bank.add(customer);
+
+        assertThat(customer.getAccount(), is(0.0));
+    }
+    
+
+}

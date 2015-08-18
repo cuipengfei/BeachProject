08bbc3a49efcb43451ae7480bf2d6b2752ca2be6
Arollalz <445223894@qq.com>
2015-08-18 13:23:21 +0800

    refactor & fasterMailSender

---
+++ Liuzhen/BugetSystem/src/Src/FasterMailSender.java
@@ -0,0 +1,10 @@
+package Src;
+
+/**
+ * Created by zhenliu on 8/18/15.
+ */
+public class FasterMailSender {
+    public void sendEmail(String receiverAddress,String mailContent){
+        System.out.println("It's faster mail sender!");
+    }
+}

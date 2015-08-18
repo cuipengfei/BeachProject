762b6e1a9a551b0efafb2dcba00e36f1820c3019
syyan <syyan@thoughtworks.com>
2015-08-18 13:07:42 +0800

    [yansiyu]add FasterMessageGateway

---
+++ syyan/src/main/java/com/thoughtworks/external/FasterMessageGateway.java
@@ -0,0 +1,8 @@
+package main.java.com.thoughtworks.external;
+
+public class FasterMessageGateway implements MessageGateway{
+    @Override
+    public void sendMessage(String receiver, String message) {
+
+    }
+}
--- syyan/test/main/java/com/thoughtworks/BankTest.java
+++ syyan/test/main/java/com/thoughtworks/BankTest.java
@@ -1,6 +1,7 @@
 package main.java.com.thoughtworks;
 
 import main.java.com.thoughtworks.exception.OverdrawException;
+import main.java.com.thoughtworks.external.FasterMessageGateway;
 import org.junit.Test;
 
 import java.util.Date;
@@ -9,33 +10,36 @@ import static main.java.com.thoughtworks.requests.CustomerRequest.deposit;
 import static main.java.com.thoughtworks.requests.CustomerRequest.withdraw;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class BankTest {
 
 
-    private EmailSender emailSender = mock(EmailSender.class);
+    private FasterMessageGateway emailSender = mock(FasterMessageGateway.class);
 
     private Bank bank = new Bank(emailSender);
 
     @Test
     public void should_add_success_when_give_the_valid_information() throws Exception {
         Customer customer = new Customer("yan123", new Date());
+
         assertTrue(bank.addCustomer(customer));
     }
 
     @Test
     public void should_add_failure_when_give_the_valid_nickName() throws Exception {
         Customer notValidCustomer = new Customer("YAN", new Date());
+
         assertFalse(bank.addCustomer(notValidCustomer));
     }
 
     @Test
     public void should_add_failure_when_give_the_same_nickName() throws Exception {
         Customer customer = new Customer("yan123", new Date());
+
         assertTrue(bank.addCustomer(customer));
         Customer existCustomer = new Customer("yan123", new Date());
+
         assertFalse(bank.addCustomer(existCustomer));
     }
 
@@ -45,6 +49,7 @@ public class BankTest {
 
         bank.addCustomer(syyan);
         bank.handleRequest(deposit(syyan, 100d));
+
         assertThat(syyan.getBalance(), is(100d));
     }
 
@@ -55,6 +60,7 @@ public class BankTest {
         bank.addCustomer(syyan);
         bank.handleRequest(deposit(syyan, 100d));
         bank.handleRequest(withdraw(syyan, 100d));
+
         assertThat(syyan.getBalance(), is(0d));
     }
 
@@ -82,7 +88,9 @@ public class BankTest {
     @Test
     public void should_use_sendMessage_when_addCustomer() {
         Customer syyan = new Customer("syyan", new Date());
+
         bank.addCustomer(syyan);
+
         verify(emailSender).sendMessage("syyan@thebank.com", "Dear syyan, Welcome to the Bank");
     }
 
@@ -92,8 +100,30 @@ public class BankTest {
 
         bank.addCustomer(syyan123);
         bank.handleRequest(deposit(syyan123, 40000d));
+
         verify(emailSender).sendMessage("manager@thebank.com", "syyan123 is now a premium customer");
     }
 
+    @Test
+    public void should_use_sendMessage_only_once_when_customer_is_deposit_or_withdraw() throws Exception, OverdrawException {
+        Customer syyan123 = new Customer("syyan123", new Date());
 
+        bank.addCustomer(syyan123);
+        bank.handleRequest(deposit(syyan123, 40000d));
+        bank.handleRequest(deposit(syyan123, 10000d));
+        bank.handleRequest(withdraw(syyan123, 50000d));
+        bank.handleRequest(deposit(syyan123, 40000d));
+
+        verify(emailSender,times(1)).sendMessage("manager@thebank.com", "syyan123 is now a premium customer");
+    }
+
+    @Test
+    public void should_not_use_sendMessage_only_once_when_customer_is_not_over_40000() throws Exception, OverdrawException {
+        Customer syyan123 = new Customer("syyan123", new Date());
+
+        bank.addCustomer(syyan123);
+        bank.handleRequest(deposit(syyan123, 10000d));
+
+        verify(emailSender,never()).sendMessage("manager@thebank.com", "syyan123 is now a premium customer");
+    }
 }

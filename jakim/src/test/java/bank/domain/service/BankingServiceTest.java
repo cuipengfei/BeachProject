4c25919a6cfbf3eab85a2dfe6f40effb1738c9d5
JakimLi <ljking7002@gmail.com>
2015-08-19 20:56:33 +0800

    add test to handle old premium customer

--- jakim/src/test/java/bank/domain/service/BankingServiceTest.java
+++ jakim/src/test/java/bank/domain/service/BankingServiceTest.java
@@ -1,6 +1,8 @@
 package bank.domain.service;
 
 import bank.domain.aggregator.Customer;
+import bank.domain.event.Event;
+import bank.domain.event.handler.Handler;
 import bank.domain.exception.OverdrawException;
 import bank.infrastructure.CustomerRepository;
 import org.junit.Before;
@@ -11,6 +13,9 @@ import java.util.Date;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Mockito.*;
 
 public class BankingServiceTest {
 
@@ -45,7 +50,37 @@ public class BankingServiceTest {
 
         assertThat(newBalance, is(10f));
         assertThat(customerRepository.findByNickname("jakim").get().getAccount().balance(), is(10f));
+    }
 
+    @Test
+    public void deposit_should_fire_new_premium_event() throws Exception {
+        Customer customer = new Customer("jakim", new Date());
+
+        Handler handler = mock(Handler.class);
+        Event.addHandler(handler);
+
+        when(handler.shouldHandle(anyObject())).thenReturn(true);
+
+        float balance = bankingService.deposit(customer, 40005f);
+
+        assertThat(balance, is(40005f));
+        verify(handler).handle(any(Event.class));
+    }
+
+    @Test
+    public void should_not_fire_event_for_a_existed_premium_customer() throws Exception {
+        Customer customer = prepareCustomer("jakim");
+        customer.upgrade();
+
+        Handler handler = mock(Handler.class);
+        Event.addHandler(handler);
+
+        when(handler.shouldHandle(anyObject())).thenReturn(true);
+
+        float balance = bankingService.deposit(customer, 40005f);
+
+        assertThat(balance, is(40005f));
+        verify(handler, never()).handle(any(Event.class));
     }
 
     @Test(expected = OverdrawException.class)
